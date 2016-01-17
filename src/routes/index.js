a8e2e113999a22a8db45c1d5275016cef9cdd7e8
Joe Smith <joesmith0488@gmail.com>
2016-01-17 23:06:29 +0000

    fixed ip bug, again

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,14 @@
 const express = require('express');
 const app = express();
 
-app.set("trust proxy", true);
+app.set('trust proxy', true);
 
 app.get('/', (req, res) => {
   let software = req.headers['user-agent'].match(/\(([^)]+)\)/i)[0]
   software = software.substring(1, software.length-1);
 
   res.json({
-    ipaddress: req.connection.remoteAddress,
+    ipaddress: req.ip,
     language: req.headers['accept-language'].split(',')[0],
     software
   });
