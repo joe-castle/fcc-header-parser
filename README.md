f32987bbdd538b08018a7cf3e2e9e2a77646580d
Joe Smith <joesmith0488@gmail.com>
2016-01-17 22:58:09 +0000

    added get to root path, which also the only peice of code

---
+++ README.md
@@ -0,0 +1,19 @@
+# FCC Request Header Parser Microservice
+
+Please see challenge page for more info: http://www.freecodecamp.com/challenges/request-header-parser-microservice
+
+```
+npm install
+node src/server.js
+
+http://localhost:3000
+```
+
+Example Output:
+```
+{
+  "ipaddress":"92.236.220.153",
+  "language":"en-US",
+  "software":"Macintosh; Intel Mac OS X 10_11_2"
+}
+```
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  let software = req.headers['user-agent'].match(/\(([^)]+)\)/i)[0]
+  software = software.substring(1, software.length-1);
+
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'].split(',')[0],
+    software
+  });
+});
+
+module.exports = app;
